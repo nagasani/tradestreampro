0093ce1f860f35d1541dc19e722c94754f86a110
nagasani <nagasani.r@gmail.com>
2024-05-28 01:09:44 -0400

    Trading App.

---
+++ tradestreampro/src/main/java/com/trading/tradestreampro/TradeStreamProApplication.java
@@ -0,0 +1,13 @@
+package com.trading.tradestreampro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeStreamProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeStreamProApplication.class, args);
+	}
+
+}
---
+++ tradestreampro/src/test/java/com/trading/tradestreampro/TradeStreamProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading.tradestreampro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeStreamProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
